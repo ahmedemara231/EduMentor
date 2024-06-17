4e2b17e6dae84ed8b5ec1bb9285f0d35c35b907b
ahmed emara <emara8028@gmail.com>
2024-06-18 02:18:31 +0300

    connect app with firebase

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:fcis_guide/view/home/home.dart';
+import 'package:fcis_guide/view_model/bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
+import 'model/local/secure_storage.dart';
+import 'model/local/shared_prefs.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await CacheHelper.getInstance().cacheInit();
+  SecureStorage.getInstance().init();
+  Bloc.observer = MyBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
